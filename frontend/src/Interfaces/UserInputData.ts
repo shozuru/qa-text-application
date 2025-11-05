2ea3e8ae6b4d16d6b75a11c514814e35e11bf1f5
Andrew Amadeo <andrewamadeo411@gmail.com>
2025-11-05 09:58:13 -0500

    Updates

--- frontend/src/Interfaces/UserInputData.ts
+++
@@ -1,9 +0,0 @@
-export interface UserInputData {
-    userAnswer: string
-    userQuestion: string
-}
-
-export interface ReceivedUserData {
-    userAnswer: string | undefined
-    userQuestion: string | undefined
-}
